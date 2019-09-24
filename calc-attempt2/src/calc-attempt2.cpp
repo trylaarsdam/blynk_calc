2a2e9ee6412a3014bcccb8045806e7e56255d46b
trylaarsdam <rylaarsdamto@tcs.timothychristian.com>
2019-09-23 21:16:54 -0500

    Main Commit

---
+++ calc-attempt2/src/calc-attempt2.cpp
@@ -0,0 +1,281 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "application.h"
+#line 1 "c:/Users/Nerdom/Desktop/calc-attempt2/src/calc-attempt2.ino"
+#include <blynk.h>
+
+void setup();
+void determine_input();
+void take_input(int value);
+void loop();
+#line 3 "c:/Users/Nerdom/Desktop/calc-attempt2/src/calc-attempt2.ino"
+#define blynk_0 V0
+#define blynk_1 V1
+#define blynk_2 V2
+#define blynk_3 V3
+#define blynk_4 V4
+#define blynk_5 V5
+#define blynk_6 V6
+#define blynk_7 V7
+#define blynk_8 V8
+#define blynk_9 V9
+#define blynk_decimal V10
+#define blynk_addition V11
+#define blynk_subtraction V12
+#define blynk_multiplication V13
+#define blynk_division V14
+#define blynk_equals V15
+#define blynk_clear V16
+#define blynk_clearEverything V17
+#define blynk_backspace V18
+#define blynk_display V19
+
+int num_0 = 0;
+int num_1 = 0;
+int num_2 = 0;
+int num_3 = 0;
+int num_4 = 0;
+int num_5 = 0;
+int num_6 = 0;
+int num_7 = 0;
+int num_8 = 0;
+int num_9 = 0;
+int decimal = 0;
+int addButton = 0;
+int subtractButton = 0;
+int multiplyButton = 0;
+int divideButton = 0;
+int equalsButton = 0;
+int clearButton = 0;
+int clearAllButton = 0;
+int backspaceButton = 0;
+
+BLYNK_WRITE(blynk_0){
+  if(param.asInt()){
+    num_0 = 1;
+  }
+  else{
+    num_0 = 0;
+  }
+}
+BLYNK_WRITE(blynk_1){
+num_1 = param.asInt();
+}
+BLYNK_WRITE(blynk_2){
+num_2 = param.asInt();
+}
+BLYNK_WRITE(blynk_3){
+num_3 = param.asInt();
+}
+BLYNK_WRITE(blynk_4){
+num_4 = param.asInt();
+}
+BLYNK_WRITE(blynk_5){
+num_5 = param.asInt();
+}
+BLYNK_WRITE(blynk_6){
+num_6 = param.asInt();
+}
+BLYNK_WRITE(blynk_7){
+num_7 = param.asInt();
+}
+BLYNK_WRITE(blynk_8){
+num_8 = param.asInt();
+}
+BLYNK_WRITE(blynk_9){
+num_9 = param.asInt();
+}
+BLYNK_WRITE(blynk_decimal){
+decimal = param.asInt();
+}
+BLYNK_WRITE(blynk_addition){
+addButton = param.asInt();
+}
+BLYNK_WRITE(blynk_subtraction){
+subtractButton = param.asInt();
+}
+BLYNK_WRITE(blynk_multiplication){
+multiplyButton = param.asInt();
+}
+BLYNK_WRITE(blynk_division){
+divideButton = param.asInt();
+}
+BLYNK_WRITE(blynk_equals){
+equalsButton = param.asInt();
+}
+BLYNK_WRITE(blynk_clear){
+clearButton = param.asInt();
+}
+BLYNK_WRITE(blynk_clearEverything){
+clearAllButton = param.asInt();
+}
+
+float value1 = 0;
+float value2 = 0;
+//double answer = 0;
+enum selected_operator {add,subtract,multiply,divide,none};
+enum selected_operator selectedOperator;
+uint16_t delay_amount = 400;
+void setup() {
+  Blynk.begin("74b8982979ac48d2b49c1e32adcf79e3",IPAddress(167,99,150,124),8080); 
+  Serial.begin(9600);
+}
+
+void determine_input(){
+  if(num_0 == 1){
+    Serial.println("0");
+    value2 = ((value2 * 10) + 0);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2);   
+    delay(delay_amount);
+  }
+  else if(num_1 == 1){
+    Serial.println("1");
+    value2 = ((value2 * 10) + 1);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2);   
+    delay(delay_amount);
+  }
+  else if(num_2 == 1){
+    Serial.println("2");
+    value2 = ((value2 * 10) + 2);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2);      
+    delay(delay_amount);
+  }
+  else if(num_3 == 1){
+    Serial.println("3");
+    value2 = ((value2 * 10) + 3);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2); 
+    delay(delay_amount);
+  }
+  else if(num_4 == 1){
+    Serial.println("4");
+    value2 = ((value2 * 10) + 4);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2); 
+    delay(delay_amount); 
+  }
+  else if(num_5 == 1){
+    Serial.println("5");
+    value2 = ((value2 * 10) + 5);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2); 
+    delay(delay_amount);
+  }
+  else if(num_6 == 1){
+    Serial.println("6");
+    value2 = ((value2 * 10) + 6);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2); 
+    delay(delay_amount);
+  }
+  else if(num_7 == 1){
+    Serial.println("7");
+    value2 = ((value2 * 10) + 7);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2); 
+    delay(delay_amount);
+  }
+  else if(num_8 == 1){
+    Serial.println("8");
+    value2 = ((value2 * 10) + 8);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2); 
+    delay(delay_amount);
+  }
+  else if(num_9 == 1){
+    Serial.println("9");
+    value2 = ((value2 * 10) + 9);
+    Serial.println((float) value2);
+    Blynk.virtualWrite(blynk_display, value2); 
+    delay(delay_amount);
+  }
+  else if(addButton == 1){
+    Serial.println("Add Selected");
+    value1 = value2;
+    Serial.println((float) value1);
+    value2 = 0;
+    Blynk.virtualWrite(blynk_display, value2);
+    selectedOperator = add;
+    delay(delay_amount);
+  }
+  else if(subtractButton == 1){
+    Serial.println("Subtract Selected");
+    value1 = value2;
+    Serial.println((float) value1);
+    value2 = 0;
+    Blynk.virtualWrite(blynk_display, value2);
+    selectedOperator = subtract;
+    delay(delay_amount);
+  }
+  else if(multiplyButton == 1){
+    Serial.println("Multiply Selected");
+    value1 = value2;
+    Serial.println((float) value1);
+    value2 = 0;
+    Blynk.virtualWrite(blynk_display, value2);
+    selectedOperator = multiply;
+    delay(delay_amount);
+  }
+  else if(divideButton == 1){
+    Serial.println("Divide Selected");
+    value1 = value2;
+    Serial.println((float) value1);
+    value2 = 0;
+    Blynk.virtualWrite(blynk_display, value2);
+    selectedOperator = divide;
+    delay(delay_amount);
+  }
+  else if(equalsButton == 1){
+    Serial.println("Equals Selected");
+    if(selectedOperator == add){
+      Serial.println("Addition in Progress");
+      value2 = value1 + value2;
+      Blynk.virtualWrite(blynk_display, value2);
+      selectedOperator = none;
+      delay(delay_amount);
+    }
+    else if(selectedOperator == subtract){
+      Serial.println("Subtraction in Progress");
+      value2 = value1 - value2;
+      Blynk.virtualWrite(blynk_display, value2);
+      selectedOperator = none;
+      delay(delay_amount);
+    }
+    else if(selectedOperator == multiply){
+      Serial.println("Multiplication in Progress");
+      Serial.println((float) value1);
+      Serial.println((float) value2);
+      value2 = value1 * value2;
+      Blynk.virtualWrite(blynk_display, value2);
+      selectedOperator = none;
+      delay(delay_amount);
+    }
+    else if(selectedOperator == divide){
+      Serial.println("Division in Progress");
+      value2 = value1 / value2;
+      Blynk.virtualWrite(blynk_display, value2);
+      selectedOperator = none;
+      delay(delay_amount);
+    }
+    else{
+      Serial.println("Equals resulted in full clear");
+      value1 = 0;
+      value2 = 0;
+      selectedOperator = none;
+      Blynk.virtualWrite(blynk_display, value2);
+      delay(delay_amount);
+    }
+  }
+}
+
+void take_input(int value){}
+
+void loop() {
+  Blynk.run();
+  determine_input();
+}
---
+++ calculator-blynk/src/calculator-blynk.cpp
@@ -0,0 +1,238 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "application.h"
+#line 1 "c:/Users/Nerdom/Desktop/calculator-blynk/src/calculator-blynk.ino"
+#include <blynk.h>
+
+void setup();
+void execute_operation();
+void display_completed(int value);
+void add();
+void subtract();
+void multiply();
+void divide();
+void clear_all();
+void clear_display();
+void backspace();
+void take_input(int input);
+void operator_decision();
+void determine_input();
+void loop();
+#line 3 "c:/Users/Nerdom/Desktop/calculator-blynk/src/calculator-blynk.ino"
+#define blynk_0 V0
+#define blynk_1 V1
+#define blynk_2 V2
+#define blynk_3 V3
+#define blynk_4 V4
+#define blynk_5 V5
+#define blynk_6 V6
+#define blynk_7 V7
+#define blynk_8 V8
+#define blynk_9 V9
+#define blynk_decimal V10
+#define blynk_addition V11
+#define blynk_subtraction V12
+#define blynk_multiplication V13
+#define blynk_division V14
+#define blynk_equals V15
+#define blynk_clear V16
+#define blynk_clearEverything V17
+#define blynk_backspace V18
+#define blynk_display V19
+
+uint16_t currentOperator = 0; //0 == null, 1 == add, 2 == sub, 3 == mult, 4 == div
+uint32_t currentValue = 0; // current value of the "screen" or current entry
+uint32_t currentValue2 = 0; //value that will be operated 
+uint32_t completedValue = 0; 
+uint16_t lastDigit = 0; //for use with backspace key
+
+void setup() {
+  Blynk.begin("74b8982979ac48d2b49c1e32adcf79e3",IPAddress(167,99,150,124),8080); 
+  Serial.begin(9600);
+}
+
+void execute_operation(){
+  if(currentOperator == 1){
+    add();
+    Serial.println("add");
+  }
+  if(currentOperator == 2){
+    subtract();
+    Serial.println("sub");
+  }
+  if(currentOperator == 3){
+    multiply();
+    Serial.println("mult");
+  }
+  if(currentOperator == 4){
+    divide();
+    Serial.println("div");
+  }
+}
+
+void display_completed(int value){
+  //displays completedValue
+  Blynk.virtualWrite(blynk_display, value);
+  Serial.println("screenwrite");
+}
+
+void add() {
+  //add numbers in array
+  completedValue = currentValue2 + currentValue;
+  display_completed(completedValue);
+}
+
+void subtract() {
+  //subtract numbers in array
+  completedValue = currentValue2 - currentValue;
+  display_completed(completedValue);
+}
+
+void multiply() {
+  //multiply numbers in array
+  completedValue = currentValue2 * currentValue;
+  display_completed(completedValue);
+}
+
+void divide() {
+  //divide numbers in array
+  completedValue = currentValue2 / currentValue;
+  display_completed(completedValue);
+}
+
+void clear_all() {
+  //clear everything (aka reset)
+  currentOperator = 0;
+  currentValue = 0;
+  currentValue2 = 0;
+  completedValue = 0;
+  display_completed(completedValue);
+  Serial.println("clearall");
+}
+
+void clear_display() {
+  //clear display
+  Serial.println("cleardisplay");
+}
+
+void backspace() {
+  //remove last digit
+  currentValue = currentValue - lastDigit;
+  currentValue = currentValue / 10;
+  display_completed(currentValue);
+  Serial.println("backspace");
+}
+
+void take_input(int input) {
+
+  //if number, append to current value
+  if(input < 10){
+    currentValue = currentValue * 10;
+    currentValue = currentValue + input;
+    lastDigit = input;
+    display_completed(currentValue);
+    Serial.println("numericalInput");
+  }
+  //-----if operator, send current value to operator value, set operator-----
+    //THIS IS DETERMINED IN operator_decision()
+
+  //-----if equals, call operator_decision-----
+    //THIS IS DETERMINED IN operator_decision()
+  
+}
+
+void operator_decision() {  //0 == null, 1 == add, 2 == sub, 3 == mult, 4 == div
+  if(blynk_addition == 1){
+    currentOperator = 1;
+    currentValue = currentValue2;
+    Serial.println("addition-set");
+  }
+  if(blynk_subtraction == 1){
+    currentOperator = 2;
+    currentValue = currentValue2;
+    Serial.println("subtraciton-set");
+  }
+  if(blynk_multiplication == 1){
+    currentOperator = 3;
+    currentValue = currentValue2;
+    Serial.println("mult-set");
+  }
+  if(blynk_division == 1){
+    currentOperator = 4;
+    currentValue = currentValue2;
+    Serial.println("div-set");
+  }
+  if(blynk_equals == 1){
+    if (currentOperator == 1){
+      add();
+      Serial.println("equals-add");
+    }
+    if (currentOperator == 2){
+      subtract();
+      Serial.println("equals-sub");
+    }
+    if (currentOperator == 3){
+      multiply();
+      Serial.println("mult-sub");
+    }
+    if (currentOperator == 4){
+      divide();
+      Serial.println("div-sub");
+    }
+  }
+  
+}
+
+void determine_input(){
+  if(blynk_0 == 1){
+    take_input(0);
+    Serial.println("0");
+  }
+  if(blynk_1 == 1){
+    take_input(1);
+    Serial.println("1");
+  }
+  if(blynk_2 == 1){
+    take_input(2);
+    Serial.println("2");
+  }
+  if(blynk_3 == 1){
+    take_input(3);
+    Serial.println("3");
+  }
+  if(blynk_4 == 1){
+    take_input(4);
+    Serial.println("4");
+  }
+  if(blynk_5 == 1){
+    take_input(5);
+    Serial.println("5");
+  }
+  if(blynk_6 == 1){
+    take_input(6);
+    Serial.println("6");
+  }
+  if(blynk_7 == 1){
+    take_input(7);
+    Serial.println("7");
+  }
+  if(blynk_8 == 1){
+    take_input(8);
+    Serial.println("8");
+  }
+  if(blynk_9 == 1){
+    take_input(9);
+    Serial.println("9");
+  }
+  else{
+    operator_decision();
+    Serial.println("operator-decision-set");
+  }
+}
+
+void loop() {
+  Blynk.run();
+  determine_input();
+}
